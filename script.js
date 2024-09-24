66b7874fa60c6ac7274475d33357c2194d7dc4ed
ArturSad753 <artur207962@student-schoolsofwestfield.org>
2024-09-24 13:46:41 +0000

    New riddle

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bellRiddle(){
-  var answer = prompt("Why is an orange like a bell? Because they both must be ...");
-  if (answer == "peeled" || answer == "pealed") alert("Correct!");
+  var answer = prompt("What needs food to survive but if it drinks it dies ...");
+  if (answer == "fire" || answer == "fire") alert("Correct!");
   else alert("Wrong, Batman! Watch the video, and try again.");
 }
