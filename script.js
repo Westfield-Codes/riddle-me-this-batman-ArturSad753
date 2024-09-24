8ba37614a1478b1179f654f930a65aed9074704a
ArturSad753 <artur207962@student-schoolsofwestfield.org>
2024-09-24 13:47:46 +0000

    commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bellRiddle(){
-  var answer = prompt("What needs food to survive but if it drinks it dies ...");
+  var answer = prompt("What needs food to survive, but if it drinks it dies ...");
   if (answer == "fire" || answer == "fire") alert("Correct!");
   else alert("Wrong, Batman! Watch the video, and try again.");
 }
